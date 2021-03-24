f6eae1e2f327fdb9666c64f074956fa54b7defb6
Andres Junge <andres@notabene.id>
2021-03-23 23:48:21 -0300

    remove console.log

--- src/cache.ts
+++ src/cache.ts
@@ -1,5 +1,8 @@
 import { DIDCache, DIDDocument } from "did-resolver";
 import Redis from "ioredis";
+import Debug from "debug";
+
+const debug = Debug("daf:cache");
 
 export function redisCache(redisUrl: string): DIDCache {
   const redis = new Redis(redisUrl);
@@ -9,11 +12,11 @@ export function redisCache(redisUrl: string): DIDCache {
       return await resolve();
 
     const cached = await redis.get(parsed.did);
-    console.log(cached);
+    debug("cached: %o", cached);
     if (cached !== null) return JSON.parse(cached);
     const doc = await resolve();
     if (doc !== null) {
-      console.log(doc);
+      debug("doc: %o", doc);
       await redis.set(parsed.did, JSON.stringify(doc));
     }
     return doc;
